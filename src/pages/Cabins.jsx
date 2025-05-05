117e213ec218a547ce726352db899c5d7736869c
Dmitry <dmitry@smilecdr.com>
2025-05-04 23:17:46 -0600

    fetching cabins and displaying in a table works

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,31 +1,19 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(() => {
-    async function fetchCabins() {
-      try {
-        const cabins = await getCabins();
-        console.log(cabins);
-      } catch (error) {
-        console.error("Failed to fetch cabins:", error);
-      }
-    }
-
-    fetchCabins();
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://dxjqhwiiaunfecmlzgvd.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg"
-        alt="cabin image"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
