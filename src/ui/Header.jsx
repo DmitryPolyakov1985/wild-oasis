82ae706bbbd519ade1d5513382b145d792264d03
Dmitry <dmitry@smilecdr.com>
2025-05-26 23:13:09 -0600

    removed unused imports

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,4 +1,4 @@
-import { styled, css } from "styled-components";
+import { styled } from "styled-components";
 import HeaderMenu from "./HeaderMenu";
 import UserAvatar from "../features/authentication/UserAvatar";
 
